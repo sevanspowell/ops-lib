e1f35ef91baae5e93e53e3e40467bdbd6488dbfc
Samuel Evans-Powell <mail@sevanspowell.net>
2020-04-23 10:37:25 +0800

    Upgrade ops-lib to use 20.03
    - Run "niv update nixpkgs -b nixos-20.03" to update to 20.03. This was
      done so we have access to the new python NixOS testing framework.
    - Support pure nix-shell:
      - Add nix to the shell buildInputs for the "nix-prefetch-url" tool.
        This is needed by "niv" when updating the nixpkgs source.
      - Set "NIX_SSL_CERT_FILE" environment variable so that
        "nix-prefetch-url" can fetch tarballs from the web.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ops-lib
 NixOps deployment configuration library for IOHK devops 
+
+## Update nixpkgs version used:
+
+For example, to update to the 'nixos-20.03' release:
+
+``` shell
+$ nix-shell --pure
+$ niv update nixpkgs -b nixos-20.03
+```
--- default.nix
+++ default.nix
@@ -5,6 +5,12 @@
   inherit nixops nginxStable nginxMainline;
   overlays = import ./overlays sourcePaths;
   shell = mkShell {
-    buildInputs = [ niv ];
+    # NIX_SSL_CERT_FILE needs to be set otherwise nix-prefetch-url
+    # throws: error: unable to download '...': Problem with the SSL CA
+    # cert (path? access rights?) (77)
+    NIX_SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    buildInputs = [ niv
+                    nix # Added for nix-prefetch-url, needed by niv to update nixpkgs
+                  ];
   };
 }
